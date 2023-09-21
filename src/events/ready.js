db907d4ca44bc00de0e21c6153a9cb694233597a
andre23k <andrepsouza2004@gmail.com>
2023-09-21 14:04:38 -0400

    added connect database

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,4 +1,19 @@
+import mongoose from "mongoose"
 import client from "../client.js"
+client.once('ready', () => readybot())
 
+function readybot() {
+    connectDatabase()
+    console.log('Event Ready | OK')
+}
 
-client.once('ready', () => console.log('Event Ready | OK'))
+function connectDatabase() {
+    mongoose.set('strictQuery', true);
+    mongoose.connect(process.env.DATABASE_TOKEN, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+        .then(() => { console.log(`Connected Database | OK!`) })
+        .catch(err => { console.log(err) })
+
+}
