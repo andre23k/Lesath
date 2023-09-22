64690641402b071582fab50222c48e5f1ffc9030
andre23k <andrepsouza2004@gmail.com>
2023-09-21 22:09:20 -0400

    fix

--- src/handlers/handler.commands.js
+++ src/handlers/handler.commands.js
@@ -1,28 +1,26 @@
-import fs from 'fs'
+import fs from 'fs';
 import { Routes } from "discord.js";
-import client from '../client.js'
+import client from '../client.js';
 
 export default async () => {
-    const SlashsArray = []
-    const subfolders = fs.readdirSync(`src/commands/`)
-    subfolders.map(async subfolder => {
-        const files = fs.readdirSync(`src/commands/${subfolder}/`)
-        files.forEach(async file => {
-            if (!file.endsWith('.js')) return;
-            const commandfile = await import(`../commands/${subfolder}/${file}`);
-            const command = commandfile.default
-          
-            if (!command?.name) return;
-            client.slashCommands.set(command.name, command);
-            SlashsArray.push(command)
-        })
-    })
+    const SlashsArray = [];
+    const subfolders = fs.readdirSync(`src/commands/`);
 
+    for (const subfolder of subfolders) {
+        const files = fs.readdirSync(`src/commands/${subfolder}/`);
+        for (const file of files) {
+            if (!file.endsWith('.js')) continue;
+            const commandfile = await import(`../commands/${subfolder}/${file}`);
+            const command = commandfile.default;
+            if (!command?.name) continue;
+            client.slashCommands.set(command.name, command);
+            SlashsArray.push(command);
+        }
+    }
     await client.rest.put(
         Routes.applicationCommands(client.user.id),
         { body: SlashsArray },
     );
-    console.log(`${client.slashCommands.size} SlashCommands | OK!`)
 
+    console.log(`${client.slashCommands.size} SlashCommands | OK!`);
 }
-
