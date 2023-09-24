75f37f740746750ee721f4335a2739ceb02a0c72
andre23k <andrepsouza2004@gmail.com>
2023-09-24 17:58:54 -0400

    .

--- src/client.js
+++ src/client.js
@@ -1,5 +1,4 @@
 import "./prototypes/index.js"
-//import { discloud } from'./discloud/discloud.js'
 process.env.TZ = "America/Sao_Paulo"
 import { Client, Collection, GatewayIntentBits } from 'discord.js'
 
