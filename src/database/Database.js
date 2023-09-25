0a7b3d7df7499c174df95c8af1350e2423d738ce
andre23k <andrepsouza2004@gmail.com>
2023-09-25 19:30:24 -0400

    finished new server event added

--- src/database/Database.js
+++ src/database/Database.js
@@ -1,4 +1,4 @@
-import { Models } from "./Models"
+import { Models } from "./Models.js"
 
 
 export default new class Database extends Models {
@@ -6,4 +6,25 @@ export default new class Database extends Models {
         super()
 
     }
+
+    async getGuild(guildId) {
+        if (!guildId) return null;
+
+        const guildData = await this.Guild.findOne({ id: guildId });
+        return guildData;
+    }
+
+    registerServer = async guild => {
+        if (!guild || !guild?.id) return
+
+        const g = await this.Guild.exists({ id: guild.id })
+
+        if (g || g?.id === guild.id) return
+
+        new this.Guild({ id: guild.id }).save()
+
+
+        return;
+    }
+
 }
--- src/events/functions/guildCreate.js
+++ src/events/functions/guildCreate.js
@@ -1,5 +1,31 @@
-import client from '../../client.js';
+import { Events } from 'discord.js'
+import client from '../../client.js'
 import Database from '../../database/Database.js'
+import { createRequire } from 'node:module'
+const require = createRequire(import.meta.url)
+const { e } = require('../../JSON/emojis.json')
 
-client.on('guildCreate', async guild => {
-})
+client.on(Events.GuildCreate, async guild => {
+    const server = await Database.getGuild(guild.id)
+    if (!server) await Database.registerServer(guild)
+
+    const owner = await client.users.fetch(guild?.ownerId || "undefined").catch(() => null)
+
+    const channelLog = client.channels.cache.get('1154599806715559966')
+    channelLog.send({
+        embeds: [{
+            color: 0x57f287,
+            title: `Servidor Adicionado`,
+            fields: [
+                {
+                    name: 'Status',
+                    value: `**Dono:** ${owner?.username || '`Not Found`'} *\`${guild?.ownerId || '0'}\`*\n**Membros:** ${guild.memberCount}`
+                },
+                {
+                    name: 'Register',
+                    value: `O servidor ${guild.name} foi registrado com sucesso!`
+                }
+            ]
+        }],
+    })
+});
